4af128d552f4b6d540d54e082c61d6e6c1bcaab0
Zensi77 <juanmaespi7@gmail.com>
2024-05-27 01:03:46 +0200

    cambio entidad almacenProducto

--- src/main/java/jEspinola/Inventario/Model/ProductoAlmacen.java
+++ src/main/java/jEspinola/Inventario/Model/ProductoAlmacen.java
@@ -4,12 +4,16 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @IdClass(ProductoAlmacen.class) // Indica que esta clase es una entidad y que la llave primaria está compuesta por dos campos
+@SQLDelete(sql = "UPDATE producto_almacen SET is_deleted = true WHERE id_producto = ? AND id_almacen = ?")
+@Where(clause = "is_deleted = false")
 public class ProductoAlmacen {
     @Id
     @ManyToOne(cascade = {CascadeType.ALL}) // Indica que esta relación es de muchos a uno
@@ -23,4 +27,7 @@ public class ProductoAlmacen {
 
     @Column(name = "cantidad")
     private int cantidad;
+
+    @Column(name = "is_deleted", columnDefinition = "boolean default false")
+    private boolean isDeleted;
 }
